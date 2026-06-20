ce582ea2f02111b5425d60ffee7d35ef7c5025a6
tygyfy <2562564@gmail.com>
2026-06-20 07:20:38 +0300

    clear

--- 10_609-52_AAS/main.js
+++
@@ -1,8 +0,0 @@
-const buttonMenu = document.querySelector('.mobile-navigation__header-button')
-const mobilenavigation = document.querySelector('.mobile-navigation')
-
-buttonMenu.addEventListener('click', buttonMenuClicked)
-
-function buttonMenuClicked(){
-    mobilenavigation.classList.toggle('opened')
-}
